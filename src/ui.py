135b01d36af3b07e772ee051478ffb14b3f4fbec
aleksandranester <167846204+aleksandranester@users.noreply.github.com>
2026-02-11 20:16:23 +0300

    ui.py

---
+++ src/ui.py
@@ -0,0 +1,20 @@
+"""
+Модуль пользовательского интерфейса
+Отвечает за взаимодействие пользователя с системой
+"""
+
+class UserInterface:
+    def __init__(self):
+        # Исходный текст пользователя
+        self.input_text = ""
+        
+        # Язык перевода по умолчанию
+        self.target_language = "en"
+
+    def get_user_input(self):
+        """Получение введённого пользователем текста"""
+        return self.input_text
+
+    def display_result(self, result):
+        """Отображение результата перевода"""
+        print("Переведённый текст:", result)
